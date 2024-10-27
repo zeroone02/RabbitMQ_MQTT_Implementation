160d46d63cb57aed5cd2d7682554146806fa2034
zeroone02@mail.ru <zeroone02@mail.ru>
2024-10-27 14:23:13 +0300

    Initial commit with Publisher and Subscriber for RabbitMQ MQTT implementation

---
+++ RabbitMQ_Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+
+class Publisher
+{
+    public static async Task Main()
+    {
+        var factory = new MqttFactory();
+        var mqttClient = factory.CreateMqttClient();
+
+        // Настройка параметров подключения
+        var mqttClientOptions = new MqttClientOptionsBuilder()
+            .WithTcpServer("localhost", 1883)
+            .Build();
+
+        // Подключение к MQTT-брокеру
+        await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+        Console.WriteLine("Publisher подключен к брокеру.");
+
+        // Публикация сообщения
+        var message = new MqttApplicationMessageBuilder()
+            .WithTopic("test/topic")
+            .WithPayload("Hello from Publisher!")
+            .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+            .Build();
+
+        await mqttClient.PublishAsync(message, CancellationToken.None);
+        Console.WriteLine("Сообщение отправлено.");
+
+        // Отключение клиента
+        await mqttClient.DisconnectAsync();
+        Console.WriteLine("Publisher отключен.");
+    }
+}
---
+++ RabbitMQ_Subscriber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using MQTTnet;
+using MQTTnet.Client;
+
+class Subscriber
+{
+    public static async Task Main()
+    {
+        var factory = new MqttFactory();
+        var mqttClient = factory.CreateMqttClient();
+
+        // Настройка параметров подключения
+        var mqttClientOptions = new MqttClientOptionsBuilder()
+            .WithTcpServer("localhost", 1883)
+            .Build();
+
+        // Обработчик для получения сообщений
+        mqttClient.ApplicationMessageReceivedAsync += e =>
+        {
+            var message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+            Console.WriteLine($"Получено сообщение: {message}");
+            return Task.CompletedTask;
+        };
+
+        // Подключение к брокеру
+        await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+        Console.WriteLine("Subscriber подключен к брокеру.");
+
+        // Подписка на тему
+        await mqttClient.SubscribeAsync("test/topic", MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce);
+        Console.WriteLine("Подписан на test/topic.");
+
+        // Ожидание для приема сообщений
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+
+        // Отключение клиента
+        await mqttClient.DisconnectAsync();
+    }
+}
